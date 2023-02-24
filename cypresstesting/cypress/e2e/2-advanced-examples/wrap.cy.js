b5540e9aa62d467757f211f1a03f05b88d59abc1
“mvaibhav131” <98808183+mvaibhav131@users.noreply.github.com>
2023-02-24 14:23:11 +0530

    added wrap function cypress

--- cypresstesting/cypress/e2e/2-advanced-examples/wrap.cy.js
+++ cypresstesting/cypress/e2e/2-advanced-examples/wrap.cy.js
@@ -5,6 +5,7 @@ it('check the wrap function',()=> {
     .then((num)=> {
         expect(num).to.equal(1)
         return 2
+        //also we can write => cy.wrap(2)
     })
     
     .then((num)=> {
