75e99756de498043d3314b27081fca35b4d2028a
“mvaibhav131” <98808183+mvaibhav131@users.noreply.github.com>
2023-03-10 10:47:03 +0530

    ;

--- cypresstesting/cypress/e2e/2-advanced-examples/window.cy.js
+++ cypresstesting/cypress/e2e/2-advanced-examples/window.cy.js
@@ -12,8 +12,8 @@ context('Window', () => {
 
   it('cy.document() - get the document object', () => {
     // https://on.cypress.io/document
-    cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
-  })
+    cy.document().should('have.property', 'charset').and('eq', 'UTF-8');
+  });
 
   it('cy.title() - get the title', () => {
     // https://on.cypress.io/title
